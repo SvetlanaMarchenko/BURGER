9d2d211204b0b83d4d2d093165a9c8c587f8444d
SvetlanaMarchenko <semusv@mail.ru>
2024-10-25 10:54:04 +0300

    Header +- looks like in Figma

--- src/app-header/app-header.jsx
+++ src/app-header/app-header.jsx
@@ -6,13 +6,13 @@ const AppHeader = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.navigationBarLeft} mr-2 mb-4 mt-4`}>
-        <BurgerIcon type="secondary" className={`${styles.icon} pl-5`} />
-        <button className="text text_type_main-default  pl-2 pr-2">Конструктор</button>
+        <BurgerIcon type="secondary" className={`${styles.icon} pl-5 pr-2`} />
+        <button className="text text_type_main-default pr-5">Конструктор</button>
       </nav>
 
       <nav className={`${styles.navigationBarLeft} mb-4 mt-4`}>
         <ListIcon type="secondary" className={`${styles.icon} pl-5 pr-2`} />
-        <button className="text text_type_main-default pl-2 pr-2">
+        <button className="text text_type_main-default pr-5">
           Лента Заказов
         </button>
       </nav>
@@ -23,7 +23,7 @@ const AppHeader = () => {
 
       <nav className={`${styles.navigationBarRight} mb-4 mt-4`}>
         <ProfileIcon type="secondary" className={`${styles.icon} pl-5 pr-2`} />
-        <button className="text text_type_main-default pl-2 pr-2">
+        <button className="text text_type_main-default pr-5">
           Личный Кабинет
         </button>
       </nav>
