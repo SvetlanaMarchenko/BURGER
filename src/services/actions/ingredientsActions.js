669f09974224b3d329a1ffa182cac00dfbfdd628
SvetlanaMarchenko <semusv@mail.ru>
2024-11-05 11:26:41 +0300

    Fixed orderActions

--- src/services/actions/ingredientsActions.js
+++ src/services/actions/ingredientsActions.js
@@ -1,25 +1,30 @@
 // services/actions/orderActions.js
-export const CREATE_INGREDIENTS_REQUEST = 'CREATE_INGREDIENTS_REQUEST'
-export const CREATE_INGREDIENTS_SUCCESS = 'CREATE_INGREDIENTS_SUCCESS'
-export const FETCH_INGREDIENTS_FAILURE = 'FETCH_INGREDIENTS_FAILURE'
+export const CREATE_INGREDIENTS_REQUEST = 'CREATE_INGREDIENTS_REQUEST';
+export const CREATE_INGREDIENTS_SUCCESS = 'CREATE_INGREDIENTS_SUCCESS';
+export const FETCH_INGREDIENTS_FAILURE = 'FETCH_INGREDIENTS_FAILURE';
 
-const ORDER_API_URL = 'https://norma.nomoreparties.space/api/ingredients'
+export const createIngredientsRequest = () => ({ type: CREATE_INGREDIENTS_REQUEST });
+export const createIngredientsSuccess = (ingredients) => ({ type: CREATE_INGREDIENTS_SUCCESS, payload: ingredients });
+export const fetchIngredientsFailure = (error) => ({ type: FETCH_INGREDIENTS_FAILURE, payload: error });
 
-export const createOrder = (ingredients) => async (dispatch) => {
-  dispatch({ type: CREATE_INGREDIENTS_REQUEST })
+const ORDER_API_URL = 'https://norma.nomoreparties.space/api/ingredients';
+
+export const createOrder = () => async (dispatch) => {
+  dispatch(createIngredientsRequest());
+
   try {
-    const response = await fetch(ORDER_API_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ ingredients }),
-    })
-    const data = await response.json()
-    if (!response.ok) throw new Error(`Error ${response.status}`)
-    dispatch({ type: CREATE_INGREDIENTS_SUCCESS, payload: data.order.number })
-  } catch (error) {
-    dispatch({ type: FETCH_INGREDIENTS_FAILURE, payload: error.message })
+    const response = await fetch(ORDER_API_URL);
+    if (!response.ok) {
+      throw new Error(`API Ошибка: ${response.status}`);
+    }
+
+    const data = await response.json();
+    dispatch(createIngredientsSuccess(data.order.number));
+  } 
+  
+  catch (error) {
+    dispatch(fetchIngredientsFailure(error.message));
   }
 };
+
 
