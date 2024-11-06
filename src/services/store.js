62487a0709849f29260cba49118d6283325b175d
SvetlanaMarchenko <semusv@mail.ru>
2024-11-06 23:16:38 +0300

    returned store to redux tools-compatible state

--- src/services/store.js
+++ src/services/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "../services/rootReducer";
+import { thunk } from "redux-thunk"; 
 
-const store = createStore(
-  rootReducer, 
-  applyMiddleware(thunk)
-);
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
+});
 
 export default store;
 
