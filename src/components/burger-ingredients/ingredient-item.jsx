7f0df9e81e9f6aca7d5e16f858a1a4d40e542ffa
SvetlanaMarchenko <semusv@mail.ru>
2024-11-07 14:40:07 +0300

    IngredientItem is working Ok

--- src/components/burger-ingredients/ingredient-item.jsx
+++ src/components/burger-ingredients/ingredient-item.jsx
@@ -4,80 +4,35 @@ import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-c
 import styles from './burger-ingredients.module.css';
 
 const IngredientItem = ({ item, index, onClick }) => {
-   const [{ isDrag }, dragRef, preview] = useDrag({
-            type: "ingredient",
-            item: { id: item._id },
-            collect: (monitor) => ({
-               isDragging: monitor.isDragging(),
-            }),
-         });
+   const [{ isDrag}, dragRef, preview] = useDrag({
+      type: "ingredient",
+      item: { id: item._id },
+      collect: (monitor) => ({
+         isDragging: monitor.isDragging(),
+      }),
+   });
 
    return (
       !isDrag && (
-      <div
-         key={item._id}
-         className={`${styles.ingredientsItem} text text_type_main-small ml-4`}
-         onClick={onClick}
-      >
-         {index === 0 && (
-            <Counter count={1} size="default" extraClass={`${styles.counterTopRight}`} color="white" />
-         )}
-         <DragPreviewImage connect={preview} src={item.image} />
-         <img ref={dragRef} src={item.image} alt={item.name} className={`${styles.backgroundImgColor} ml-4 mb-1 mr-4 mt-6`}/>
+         <div
+            key={item._id}
+            className={`${styles.ingredientsItem} text text_type_main-small ml-4`}
+            onClick={onClick}
+         >
+            {index === 0 && (
+               <Counter count={1} size="default" extraClass={`${styles.counterTopRight}`} color="white" />
+            )}
+            <DragPreviewImage connect={preview} src={item.image} />
+            <img ref={dragRef} src={item.image} alt={item.name} className="ml-4 mb-1 mr-4 mt-6" />
 
-         <p className={`${styles.priceItem} mb-1 text text_type_digits-default`}>
-            {item.price}
-            <CurrencyIcon type="primary" />
-         </p>
-         <p>{item.name}</p>
-      </div>
-   ))
+            <p className={`${styles.priceItem} mb-1 text text_type_digits-default`}>
+               {item.price}
+               <CurrencyIcon type="primary" />
+            </p>
+            <p>{item.name}</p>
+         </div>
+      )
+   );
 };
 
-
 export default IngredientItem;
-
-
-
-// import React from 'react';
-// import { useDrag, DragPreviewImage } from 'react-dnd';
-// import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
-// import styles from './burger-ingredients.module.css';
-
-// const IngredientItem = ({ item, index, onClick }) => {
-//    const [{ isDragging }, dragRef, preview] = useDrag({
-//       type: "ingredient",
-//       item: { id: item._id },
-//       collect: (monitor) => ({
-//          isDragging: monitor.isDragging(),
-//       }),
-//    });
-
-//    return (
-//       <div
-//          key={item._id}
-//          className={`${styles.ingredientsItem} text text_type_main-small ml-4`}
-//          onClick={onClick}
-//          ref={dragRef}
-//       >
-//          {index === 0 && (
-//             <Counter count={1} size="default" extraClass={`${styles.counterTopRight}`} color="white" />
-//          )}
-//          <DragPreviewImage connect={preview} src={item.image} />
-
-//          <div className={`${styles.backgroundImgColor} ml-4 mb-1 mr-4 mt-6`}>
-//             <img src={item.image} alt={item.name} />
-//          </div>
-         
-//          <p className={`${styles.priceItem} mb-1 text text_type_digits-default`}>
-//             {item.price}
-//             <CurrencyIcon type="primary" />
-//          </p>
-//          <p>{item.name}</p>
-//       </div>
-//    );
-// };
-
-// export default IngredientItem;
-
-
