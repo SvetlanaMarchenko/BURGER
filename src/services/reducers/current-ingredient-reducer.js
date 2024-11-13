124f269e1e76284442d29251b1f91356436b64c4
SvetlanaMarchenko <semusv@mail.ru>
2024-11-13 16:15:02 +0300

    Corrected the IngredientDetails. WIP

--- src/services/reducers/current-ingredient-reducer.js
+++ src/services/reducers/current-ingredient-reducer.js
@@ -1,31 +1,16 @@
 import {
   SET_CURRENT_INGREDIENT,
   CLEAR_CURRENT_INGREDIENT,
-  // FETCH_INGREDIENT_REQUEST,
-  // FETCH_INGREDIENT_SUCCESS,
-  // FETCH_INGREDIENT_FAILURE
 } from '../actions/current-ingredient-actions';
 
-// const initialState = {
-//   currentIngredient: null,
-//   loading: false,
-//   error: null
-// };
+const initialState = null;
 
-const initialCurrentIngredientState = null;
-
-const currentIngredientsReducer = (currentIngredientState = initialCurrentIngredientState, action) => {
+const currentIngredientsReducer = (currentIngredientState = initialState, action) => {
   switch (action.type) {
     case SET_CURRENT_INGREDIENT:
       return action.payload;
     case CLEAR_CURRENT_INGREDIENT:
       return null ;
-    // case FETCH_INGREDIENT_REQUEST:
-    //   return { ...state, loading: true };
-    // case FETCH_INGREDIENT_SUCCESS:
-    //   return { ...state, loading: false, currentIngredient: action.payload };
-    // case FETCH_INGREDIENT_FAILURE:
-    //   return { ...state, loading: false, error: action.error };
     default:
       return currentIngredientState;
   }
