2a52748b42da2983976d36e580e48328f70ad7df
SvetlanaMarchenko <semusv@mail.ru>
2024-11-19 12:32:56 +0300

    Done with layout of LoginPage. WIP

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,6 +4,7 @@ import BurgerConstructor from '../burger-constructor/burger-constructor';
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LoginPage } from '../../pages/login-page/login-page';
 
 import styles from './app.module.css';
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Route for the main layout */}
         <Route path="/" element={
           <div className={`${styles.appLayout}`}>
             <AppHeader />
@@ -23,7 +25,10 @@ function App() {
           </div>
         } />
         
-        {/* <Route path="/about" element={<AboutPage />} /> */}
+        {/* Route for login page */}
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* Other routes can be added here */}
         {/* <Route path="/contact" element={<ContactPage />} /> */}
       </Routes>
     </Router>
@@ -31,7 +36,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
--- src/pages/login-page/login-page.jsx
+++ src/pages/login-page/login-page.jsx
@@ -1,26 +1,68 @@
-
-import AppHeader from '../app-header/app-header/';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {PasswordInput,EmailInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './login-page.module.css';
+import AppHeader from '../../components/app-header/app-header'; // Import AppHeader component
 
-function loginPage() {
 
+export function LoginPage() {
+  const [password, setPassword] = useState('');
+  const [value, setValue] = React.useState('bob@example.com')
+  const onChange = e => {
+    setValue(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
   return (
-    <div className={`${styles.appLayout}`}>
-      <AppHeader />
-      <DndProvider backend={HTML5Backend}>
-      <div className={`${styles.ingredientsBox}`}>
-          <BurgerIngredients/>
-          <BurgerConstructor className={` ml-10 mr-4`}/>
+    <div className={styles.loginLayout}>
+      <AppHeader/> 
+      <div>
+        <div className={styles.container}>
+          <form className={styles.form}>
+            <h1 className={`text text_type_main-medium mb-6`}>Вход</h1>
+            <EmailInput
+                onChange={onChange}
+                value={value}
+                name={'email'}
+                isIcon={false}
+                extraClass="mb-6"
+              /> 
+              <PasswordInput
+                onChange={handlePasswordChange}
+                value={password}
+                name="password"
+                extraClass="mb-6"
+              />
+            <Button
+              htmlType="button"
+              type="primary"
+              size="medium"
+              // onClick={handleCreateOrder}
+              extraClass="mb-20"
+            >
+              Войти
+            </Button>
+            
+            <div className={`${styles.newPerson}`}>
+            <p className="text text_type_main-default text_color_inactive mb-4"> Вы новый пользователь? </p> 
+            <Link to="/list" className={styles.link}>
+              Зарегистрироваться
+            </Link>
+            </div>
+
+            <div className={`${styles.newPerson}`}>
+            <p className="text text_type_main-default text_color_inactive "> Забыли пароль? </p> 
+            <Link to="/list" className={styles.link}>
+              Восстановить пароль
+            </Link>
+            </div>
+            
+          </form>
+        </div>
       </div>
-      </DndProvider>
     </div>
   );
 }
-
-export default loginPage;
