a652ec9a49e566ca728275dc993dcbc0185a8a2d
SvetlanaMarchenko <semusv@mail.ru>
2024-12-09 10:57:47 +0300

    AppHeader component converted to the TSX

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './app-header.module.css';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
@@ -8,34 +7,20 @@ const AppHeader = () => {
     <header className={styles.header}>
       <nav className={`${styles.navigationBarLeft} mr-2 mb-4 mt-4`}>
         <BurgerIcon type="secondary" className={`${styles.icon} pl-5 pr-2`} />
-
-
         <NavLink 
           to="/"
           className={({ isActive }) => 
             isActive ? `${styles.active1} text text_type_main-default` : "text text_type_main-default text_color_inactive pr-5"
-          }
-        > 
+          }> 
            Конструктор
         </NavLink>
-
-
       </nav>
 
       <nav className={`${styles.navigationBarLeft} mb-4 mt-4`}>
         <ListIcon type="secondary" className={`${styles.icon} pl-5 pr-2`} />
-
         <button className="text text_type_main-default text_color_inactive pr-5">
           Лента Заказов
         </button>
-        {/* <NavLink 
-          to="/"
-          className={({ isActive }) => 
-            isActive ? `${styles.active1} text text_type_main-default` : "text text_type_main-default text_color_inactive pr-5"
-          }
-        >
-           Лента заказов
-        </NavLink> */}
       </nav>
 
       <div className={styles.logoContainer}>
@@ -48,9 +33,8 @@ const AppHeader = () => {
           to="/profile"
           className={({ isActive }) => 
             isActive ? `${styles.active1} text text_type_main-default` : "text text_type_main-default text_color_inactive pr-5"
-          }
-        >
-           Личный Кабинет
+          }>
+          Личный Кабинет
         </NavLink>
       </nav>
     </header>
