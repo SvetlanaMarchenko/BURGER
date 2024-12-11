d2cb8e884b8832907599a496aaf2f6ab5232c6e5
SvetlanaMarchenko <semusv@mail.ru>
2024-12-11 15:18:31 +0300

    Converted ingredients reducer to TS

--- src/services/actions/ingredients-actions.ts
+++ src/services/actions/ingredients-actions.ts
@@ -5,15 +5,24 @@ import { ThunkAction } from 'redux-thunk';
 import { Action } from 'redux';
 import { RootState, AppDispatch } from '../store.js';
 
-
 export const FETCH_INGREDIENTS_REQUEST = 'FETCH_INGREDIENTS_REQUEST';
 export const FETCH_INGREDIENTS_SUCCESS = 'FETCH_INGREDIENTS_SUCCESS';
 export const FETCH_INGREDIENTS_FAILURE = 'FETCH_INGREDIENTS_FAILURE';
 
-export const fetchIngredientsRequest = () : Action => ({ type: FETCH_INGREDIENTS_REQUEST });
-export const fetchIngredientsSuccess = (ingredients: Ingredients) => ({ type: FETCH_INGREDIENTS_SUCCESS, payload: ingredients });
-export const fetchIngredientsFailure = (error: string) => ({ type: FETCH_INGREDIENTS_FAILURE, payload: error });
 
+export const fetchIngredientsRequest = (): Action => ({
+  type: FETCH_INGREDIENTS_REQUEST
+});
+
+export const fetchIngredientsSuccess = (ingredients: Ingredients): { type: typeof FETCH_INGREDIENTS_SUCCESS, payload: Ingredients } => ({
+  type: FETCH_INGREDIENTS_SUCCESS,
+  payload: ingredients
+});
+
+export const fetchIngredientsFailure = (error: string): { type: typeof FETCH_INGREDIENTS_FAILURE, payload: string } => ({
+  type: FETCH_INGREDIENTS_FAILURE,
+  payload: error
+});
 
 export const fetchDataIngredients = (): ThunkAction<void, RootState, unknown, Action<string>> => {
   return (dispatch) => {
@@ -21,7 +30,7 @@ export const fetchDataIngredients = (): ThunkAction<void, RootState, unknown, Ac
 
     requestFromApi('/ingredients')
       .then(json => dispatch(fetchIngredientsSuccess(json.data)))
-      .catch(error => dispatch(fetchIngredientsFailure(error)));
+      .catch(error => dispatch(fetchIngredientsFailure(error.toString())));
   };
 };
 
@@ -44,5 +53,6 @@ export const fetchDataIngredientsAndSetCurrent = (ingredient_id: string) => {
       .catch(error => dispatch(fetchIngredientsFailure(error)));
   };
 };
+
 
 
