607a7809bd06aa8f0643600f7d3ca0f38d95b17d
SvetlanaMarchenko <semusv@mail.ru>
2024-12-14 00:38:38 +0300

    rootElement converted to TSX

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import App from './components/app/app.tsx';
+import App from './components/app/app.js';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './services/store.ts';
+import store from './services/store.js';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')!;
+
+createRoot(rootElement).render(
   <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
     <Provider store={store}>
       <App />
