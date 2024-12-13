2c258aa0fae6b9880d2db35e55aaa94a928501d6
SvetlanaMarchenko <semusv@mail.ru>
2024-12-13 16:09:30 +0300

    IngredientItem now converted to TSX

--- src/components/burger-ingredients/ingredient-item.tsx
+++ src/components/burger-ingredients/ingredient-item.tsx
@@ -1,45 +1,48 @@
-import React from 'react';
 import { useDrag, DragPreviewImage } from 'react-dnd';
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-ingredients.module.css';
-import {useSelector } from 'react-redux';
-import PropTypes from 'prop-types';
-import { IngredientType } from '../../utils/types';
+import { useSelector } from 'react-redux';
+import { Ingredient } from '../../utils/types/ingredients';
+import { RootState } from '../../services/store';
 
-const IngredientItem = ({ item, onClick }) => {
-  const ingredientsInConstructor = useSelector((state) => state.burgerConstructor.ingredients);
-  const bunInConstructor = useSelector((state) => state.burgerConstructor.bun);
-  
-  const ingredientInConstructorCount = item.type === 'bun' 
+export interface IngredientItemProps {
+  item: Ingredient;
+  onClick: (item: Ingredient) => void;
+}
+
+const IngredientItem: React.FC<IngredientItemProps> = ({ item, onClick }) => {
+  const ingredientsInConstructor = useSelector((state: RootState) => state.burgerConstructor.ingredients);
+  const bunInConstructor = useSelector((state: RootState) => state.burgerConstructor.bun);
+
+  const ingredientInConstructorCount = item.type === 'bun'
     ? (bunInConstructor && bunInConstructor._id === item._id ? 1 : 0)
     : ingredientsInConstructor.filter(ingredient => ingredient._id === item._id).length;
 
-  const [{ isDrag }, dragRef, preview] = useDrag({
+  const [{ isDragging }, dragRef, preview] = useDrag(() => ({
     type: 'item',
     item: item,
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
-  });
+  }));
 
   const handleClick = () => {
-    onClick(item);  
+    onClick(item);
   };
 
   return (
     <>
-      {!isDrag && (
+      {!isDragging && (
         <div
           key={item._id}
           className={`${styles.ingredientsItem} text text_type_main-small ml-4`}
           onClick={handleClick}
         >
           {ingredientInConstructorCount > 0 && (
-            <Counter 
-              count={ingredientInConstructorCount} 
-              size="default" 
-              extraClass={`${styles.counterTopRight}`} 
-              color="white" 
+            <Counter
+              count={ingredientInConstructorCount}
+              size="default"
+              extraClass={styles.counterTopRight}
             />
           )}
 
@@ -58,11 +61,6 @@ const IngredientItem = ({ item, onClick }) => {
       )}
     </>
   );
-};
-
-IngredientItem.propTypes = {
-  item: IngredientType,
-  onClick: PropTypes.func
 };
 
 export default IngredientItem;
