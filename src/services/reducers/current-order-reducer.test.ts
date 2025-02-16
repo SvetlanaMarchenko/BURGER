b99cae316be1f7976db0a940b724369ffe4b80ca
SvetlanaMarchenko <semusv@mail.ru>
2025-02-16 23:41:44 +0300

    done with ordersReducer Tests

--- src/services/reducers/current-order-reducer.test.ts
+++ src/services/reducers/current-order-reducer.test.ts
@@ -27,7 +27,7 @@ describe('ordersReducer', () => {
     expect(ordersReducer(initialState, action)).toEqual(expectedState);
   });
 
-  it('should handle FETCH_ORDERS_REQUEST case', () => {
+  it('should handle FETCH_ORDERS_SUCCESS case', () => {
     const mockOrders = {
         _id: "678d702b133acd001be4b65a",
         ingredients: [
@@ -56,31 +56,30 @@ describe('ordersReducer', () => {
     expect(ordersReducer(initialState, action)).toEqual(expectedState);
   });
 
+    it('should handle FETCH_ORDERS_FAILURE case', () => {
+        const errorMessage = 'Oooooops errroooorrr';
+
+        const action = {
+            type: FETCH_ORDERS_FAILURE,
+            payload: errorMessage,
+          };
+        const expectedState = {
+            ...initialState,
+            loading: false,
+            error: errorMessage, 
+
+        }
+
+    expect(ordersReducer(initialState, action)).toEqual(expectedState);
+  });
+
 
 });
 
 
 
 
-//   it('should handle FETCH_ORDERS_SUCCESS action', () => {
-//     const mockOrders = [
-//       { id: 1, name: 'Order 1' },
-//       { id: 2, name: 'Order 2' },
-//     ];
 
-//     const action = {
-//       type: FETCH_ORDERS_SUCCESS,
-//       payload: mockOrders,
-//     };
-
-//     const expectedState = {
-//       orders: mockOrders,
-//       loading: false,
-//       error: null,
-//     };
-
-//     expect(ordersReducer(initialState, action)).toEqual(expectedState);
-//   });
 
 //   it('should handle FETCH_ORDERS_FAILURE action', () => {
 //     const errorMessage = 'Error fetching orders';
