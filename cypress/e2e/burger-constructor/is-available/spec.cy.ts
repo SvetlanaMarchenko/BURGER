9575fd386f02685ba1ccbe23947cd8511dea1b48
SvetlanaMarchenko <semusv@mail.ru>
2025-02-25 16:15:11 +0300

    In cypress files we don't have repeatable selectors

--- cypress/e2e/burger-constructor/is-available/spec.cy.ts
+++ cypress/e2e/burger-constructor/is-available/spec.cy.ts
@@ -1,32 +1,43 @@
 describe('burger-constructor is working correctly', function() {
+  const ingredientSelector = '[draggable="true"]'
+  const closeButtonSelector = '[data-cy=on-close]'
+  const constructorDropTarget= '[data-cy=constructorDropTarget]'
+  const orderNumberSelector = '[data-cy="order-number"]'
+  const nutritionFactSelector = '[data-cy=nutrition-fact]'
+  const emailInputSelector = '[data-cy="E-mail"]'
+  const passwordInputSelector = '[data-cy="password"]'
+  const loginOkSelector = '[data-cy="enter"]'
+  const orderCreationButtonSelector = '[data-cy="orderCreation"]'
+
   before(function() {
     cy.visit('http://localhost:5173');
+
   });
 
   it('should drag ingredient to constructor and drop', function() {
 
     // Open modal window with order details
-    cy.get('[draggable="true"]').first().click()
-    cy.get('[data-cy=nutrition-fact]');
-    cy.get('[data-cy=on-close]') 
+    cy.get(ingredientSelector).first().click()
+    cy.get(nutritionFactSelector);
+    cy.get(closeButtonSelector) 
     .click(); 
-    cy.get('[data-cy=on-close]').should("not.exist")
+    cy.get(closeButtonSelector).should("not.exist")
 
 
   // Drag and drop for ingredients and bun seperatly
   // Bun:
-    cy.get('[draggable="true"]').first()
+    cy.get(ingredientSelector).first()
       .trigger('dragstart');
 
-    cy.get('[data-cy=constructorDropTarget]')
+    cy.get(constructorDropTarget)
       .trigger('dragover')
       .trigger('drop');
 
   // other ingredients:
-    cy.get('[draggable="true"]').eq(5)
+    cy.get(ingredientSelector).eq(5)
       .trigger('dragstart');
 
-    cy.get('[data-cy=constructorDropTarget]')
+    cy.get(constructorDropTarget)
       .trigger('dragover')
       .trigger('drop');
 
@@ -36,20 +47,20 @@ describe('burger-constructor is working correctly', function() {
 
     // Log in 
     cy.log('Ввод e-mail');
-    cy.get('[data-cy="E-mail"]').type('gfhjgfjh@mail.ru', { timeout: 3000 });
+    cy.get(emailInputSelector).type('gfhjgfjh@mail.ru', { timeout: 3000 });
 
     cy.log('Ввод пароля');
-    cy.get('[data-cy="password"]').type('000000', { timeout: 3000 });
-    cy.get('[data-cy="enter"]').click();
+    cy.get(passwordInputSelector).type('000000', { timeout: 3000 });
+    cy.get(loginOkSelector).click();
 
     //order-creation process
-    cy.get('[data-cy="orderCreation"]').click();
+    cy.get(orderCreationButtonSelector).click();
     cy.log('Waiting for createOrder request...');
-    cy.get('[data-cy="order-number"]', {timeout: 30000})
+    cy.get(orderNumberSelector, {timeout: 30000})
 
-    cy.get('[data-cy="order-number"]').contains("Идентификатор заказа")
-    cy.get('[data-cy=on-close]')
+    cy.get(orderNumberSelector).contains("Идентификатор заказа")
+    cy.get(closeButtonSelector)
     .click();
-    cy.get('[data-cy="order-number"]').should("not.exist")
+    cy.get(orderNumberSelector).should("not.exist")
   });
 });
